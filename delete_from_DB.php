91e571f9b2d63b7f6866cc4b299252bbf31b40eb
Artur Khotenko <megamufio@gmail.com>
2016-01-08 10:07:13 +0000

    removed white spaces

--- delete_from_DB.php
+++ delete_from_DB.php
@@ -24,4 +24,3 @@ require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
 $tablename = "local_eexcess_interests";
 $id = $_POST['catid'];
 $DB->delete_records($tablename,array("id"=>$id));
-
