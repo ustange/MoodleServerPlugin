1a6dce9b190db4e5b304ebf0a9cd1cffda3d0c2e
Artur Khotenko <megamufio@gmail.com>
2016-01-21 18:05:24 +0000

    added description for file

--- lang/en/local_eexcess.php
+++ lang/en/local_eexcess.php
@@ -15,6 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * Language strings
+ * 
  * @package    local_eexcess
  * @copyright  bit media e-solutions GmbH <gerhard.doppler@bitmedia.cc>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
